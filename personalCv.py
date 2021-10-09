abeaf989e7f9b83c356c6ec2be0945bc6b19ad01
giandata <happydaysbcn2017@gmail.com>
2021-10-09 16:45:33 +0200

    subtitle

--- personalCv.py
+++ personalCv.py
@@ -5,7 +5,7 @@ col1, col2, col3 = st.columns([5,2,1])
 
 with col1:
     st.title(" Giancarlo Di Donato")
-    st.markdown('''#### Business Analyst''')
+    st.markdown('''#### <u>Business Analyst</u>''')
 
 with col2:
     st.image("pic.png")
