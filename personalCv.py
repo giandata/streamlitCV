8901e13c0c252f5dfec06251bd0ad428849f316e
giandata <happydaysbcn2017@gmail.com>
2021-10-09 16:48:49 +0200

    allow html

--- personalCv.py
+++ personalCv.py
@@ -5,7 +5,7 @@ col1, col2, col3 = st.columns([5,2,1])
 
 with col1:
     st.title(" Giancarlo Di Donato")
-    st.markdown('''<h4 style="text-decoration:underline">Businness Analyst</h4>''',allow_unsafe_html = True )
+    st.markdown('#### Businness Analyst' )
 
 with col2:
     st.image("pic.png")
